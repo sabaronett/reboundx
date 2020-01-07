007ef6d61013176c44a45afabf3b83c59a2549e0
Noah Ferich <54567748+Nofe4108@users.noreply.github.com>
2020-01-06 16:25:29 -0800

    Updating the effect so it will run with the example given with it

--- examples/mass_loss_test_run/mass_loss_test_run.c
+++ examples/mass_loss_test_run/mass_loss_test_run.c
@@ -5,28 +5,29 @@
 
 
 int main(int argc, char* argv[]) {
-        struct reb_simulation* r = reb_create_simulation();
-        r->dt = 0.1;
-        r->integrator = REB_INTEGRATOR_WHFAST;
+        struct reb_simulation* sim = reb_create_simulation();
+        sim->dt = 0.1;
+        sim->integrator = REB_INTEGRATOR_WHFAST;
 
         struct reb_particle p1 = {0};
         p1.m = 1.;
-        reb_add(r, p1);
+        reb_add(sim, p1);
 
         struct reb_particle p2 = {0};
         p2.x = 1;
         p2.vy = 1;
-        p2.m = 0.;
-        reb_add(r, p2);
-        
-        struct rebx_extras* rebx = rebx_attach(r);  // first initialize rebx
+        p2.m = .01;
+        reb_add(sim, p2);
+
+        struct rebx_extras* rebx = rebx_attach(sim);  // first initialize rebx
         struct rebx_operator* mass_loss_test = rebx_load_operator(rebx, "mass_loss_test"); // add our new force
         rebx_add_operator(rebx, mass_loss_test);
-        reb_move_to_com(r);
-        reb_integrate(r,10000.);
+        reb_move_to_com(sim);
+        reb_integrate(sim,10000.);
+        rebx_free(rebx);
         
         printf("%f\n",p1.m);
-        printf("%f\n", r->t);
+        printf("%f\n", sim->t);
         printf("%f", p2.x);
         
 }
